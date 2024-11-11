839ee5ba2941375368c54bb532689119c933c99f
Moreno <161411179+MorenoHilbran@users.noreply.github.com>
2024-11-11 10:15:22 +0700

    Pickup.jsx

--- src/components/Pickup.jsx
+++ src/components/Pickup.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import row from '../assets/row.png'
-import row2 from '../assets/row2.png'
+import Row from '../assets/Row.png'
+import Row2 from '../assets/Row2.png'
 
 const Pickup = () => {
     return (
     <section>
         <h2 className='text-teal-400 font-bold text-3xl text-center p-5 mt-20'>PICKUP FOOD WASTE SERVICES</h2>
         <div className='p-32'>
-        <img src={row} alt='Row.png' className='w-full'/>
-        <img src={row2} alt='Row2.png' className='w-full'/>
+        <img src={Row} alt='Row.png' className='w-full'/>
+        <img src={Row2} alt='Row2.png' className='w-full'/>
         </div>
     </section>
     )
